6021cffbd243a2973a27a081c31b41867503030c
zaaGallagher <mrezafahlevi62@gmail.com>
2025-10-08 09:35:44 +0700

    Percobaan 3

---
+++ nestedUjianSkripsi17.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class nestedUjianSkripsi17 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String pesan;
+        System.out.println("Apakah mahasiswa sudah bebas kompen? (Ya/Tidak)");
+        // fungsi trim() berfungsi untuk menghapus spasi sebelum dan setelah string
+        String bebasKompen = sc.nextLine().trim();
+
+        System.out.println("Masukkan jumlah log bimbingan Pembimbing 1: ");
+        int bimbinganP1 = sc.nextInt();
+        System.out.println("Masukkan jumlah log bimbingan Pembimbing 2: ");
+        int bimbinganP2 = sc.nextInt();
+
+        if (bebasKompen.equalsIgnoreCase("Ya")) {
+            if(bimbinganP1 >= 8 && bimbinganP2 >= 4) {
+                pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
+            } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
+                pesan = "Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
+            } else if (bimbinganP1 < 8) {
+                pesan = "Gagal! Log bimbingan P1 belum mencapai 8 kali";
+            } else {
+                pesan = "Gagal! Log bimbingan P2 belum mencapai 4 kali";
+            }
+        } else {
+            pesan = "Gagal! Mahasiswa masih memiliki tanggungan kompen";
+        }
+        System.out.println(pesan);
+
+
+    }
+}
